765dc06e3a0481b05cbf0fb00ab225d82282c56a
Henry Vaughn <hcvaughn@unca.edu>
2024-09-03 12:11:59 -0400

    Modified the inheritance challenge

--- asgn02-inheritance/challenge-02-inheritance.php
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -2,12 +2,22 @@
 
 // Parent class
 class Animal {
-    // Class variable
-    public $name;
+    // Protected class variable
+    protected $name;
 
     // Constructor to initialize the name
     public function __construct($name) {
+        $this->setName($name);
+    }
+
+    // Protected setter for name
+    public function setName($name) {
         $this->name = $name;
+    }
+
+    // Public getter for name
+    public function getName() {
+        return $this->name;
     }
 
     // Class method
@@ -17,19 +27,29 @@ class Animal {
 
     // Method to display basic info
     public function getInfo() {
-        return "This is an animal named $this->name.";
+        return "This is an animal named " . $this->getName() . ".";
     }
 }
 
 // Subclass 1
 class Dog extends Animal {
-    // Additional attribute specific to Dog
-    public $breed;
+    // Private attribute specific to Dog
+    private $breed;
 
     // Constructor to initialize name and breed
     public function __construct($name, $breed) {
         parent::__construct($name);
+        $this->setBreed($breed);
+    }
+
+    // Setter for breed
+    public function setBreed($breed) {
         $this->breed = $breed;
+    }
+
+    // Getter for breed
+    public function getBreed() {
+        return $this->breed;
     }
 
     // Overriding the makeSound method
@@ -39,19 +59,29 @@ class Dog extends Animal {
 
     // Overriding the getInfo method to include breed information
     public function getInfo() {
-        return "This is a $this->breed dog named $this->name.";
+        return "This is a " . $this->getBreed() . " dog named " . $this->getName() . ".";
     }
 }
 
 // Subclass 2
 class Cat extends Animal {
-    // Additional attribute specific to Cat
-    public $color;
+    // Private attribute specific to Cat
+    private $color;
 
     // Constructor to initialize name and color
     public function __construct($name, $color) {
         parent::__construct($name);
+        $this->setColor($color);
+    }
+
+    // Setter for color
+    public function setColor($color) {
         $this->color = $color;
+    }
+
+    // Getter for color
+    public function getColor() {
+        return $this->color;
     }
 
     // Overriding the makeSound method
@@ -61,7 +91,7 @@ class Cat extends Animal {
 
     // Overriding the getInfo method to include color information
     public function getInfo() {
-        return "This is a $this->color cat named $this->name.";
+        return "This is a " . $this->getColor() . " cat named " . $this->getName() . ".";
     }
 }
 
@@ -70,10 +100,10 @@ $dog = new Dog("Buddy", "Golden Retriever");
 $cat = new Cat("Whiskers", "black");
 
 // Demonstrate that subclasses inherit from the parent class and override methods
-echo $dog->getInfo() . '<br>'; 
-echo $dog->makeSound() . '<br>'; 
+echo $dog->getInfo() . '<br>';
+echo $dog->makeSound() . '<br>';
 
-echo $cat->getInfo() . '<br>'; 
-echo $cat->makeSound() . '<br>'; 
+echo $cat->getInfo() . '<br>';
+echo $cat->makeSound() . '<br>';
 
 ?>
