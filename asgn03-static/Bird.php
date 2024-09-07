a2405a3a82edb53c2f1d08639e30774ee7478e92
Henry Vaughn <hcvaughn@unca.edu>
2024-09-07 18:35:08 -0400

    Modified Bird Class

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,31 +1,62 @@
 <?php
 
 class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+    
+    // Public properties for Bird details
+    public $habitat;
+    public $food;
+    public $nesting = "tree";
+    public $conservation;
+    public $song = "chirp";
+    
+    // Protected property, accessible only within the class and its subclasses
+    protected $flying = "yes";
+    
+    // Static properties
+    public static $instanceCount = 0;
+    public static $eggNum = 0; // Default egg number for birds
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
+    // Static method to create a new instance of Bird
+    public static function create() {
+        $className = get_called_class(); // Retrieve the name of the calling class
+        $obj = new $className; // Create a new instance of the calling class
+        self::$instanceCount++; // Increment the instance count
+        return $obj; // Return the created instance
+    }
+
+    // Method to check if the bird can fly using ternary operator
+    public function canFly() {
+        return $this->flying == "yes" ? "bird can fly" : "cannot fly and is stuck on the ground";
     }
 }
 
 class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+    // Public properties specific to YellowBelliedFlyCatcher
+    public $name = "yellow-bellied flycatcher";
+    public $diet = "mostly insects.";
+    public $song = "flat chilk";
+    
+    // Static property to override eggNum for YellowBelliedFlyCatcher
+    public static $eggNum = "3-4, sometimes 5";
 }
 
 class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+    // Public properties specific to Kiwi
+    public $name = "kiwi";
+    public $diet = "omnivorous";
+    
+    // Overriding the flying property
+    protected $flying = "no";
 }
+
+// Example usage
+$bird1 = Bird::create();
+echo "Bird instance count: " . Bird::$instanceCount . "<br>";
+
+$kiwi = Kiwi::create();
+echo "Kiwi can fly: " . $kiwi->canFly() . "<br>";
+
+$flyCatcher = YellowBelliedFlyCatcher::create();
+echo "FlyCatcher eggNum: " . YellowBelliedFlyCatcher::$eggNum . "<br>";
+
+?>
